e0fdc11ab53466959940ef435044b7fa10fcb504
hyoeunkim126 <93234816+hyoeunkim126@users.noreply.github.com>
2022-02-04 15:55:55 +0900

    0204

--- js/hyon.js
+++ js/hyon.js
@@ -1,20 +1,20 @@
 // 타이틀 나타나기
 $(document).ready(function(){
   $('.paper').css("transform","translateY(0px)").css("opacity","1");
-    $(window).scroll(function(){
-      $('.paper').css("transform","translateY(0px)").css("opacity","0");
+  //   $(window).scroll(function(){
+  //     $('.paper').css("transform","translateY(0px)").css("opacity","0");
 
-      var h1=$(this).scrollTop();
-      if(h1==0){
-        $('.paper').css("transform","translateY(0px)").css("opacity","1");
-      } else{
-        $('.paper').css("transform","translateY(0px)").css("opacity","0");
+  //     var h1=$(this).scrollTop();
+  //     if(h1==0){
+  //       $('.paper').css("transform","translateY(0px)").css("opacity","1");
+  //     } else{
+  //       $('.paper').css("transform","translateY(0px)").css("opacity","0");
 
-      }
+  //     }
       
       
 
-  });
+  // });
 });
 
 
@@ -30,8 +30,11 @@ var currentScrollTop=0;
         currentScrollTop = $(window).scrollTop();
         if(currentScrollTop > 10) {
             $('header').addClass('active_bg');
+            $('.title p').css('color', '#000');
             }else{
             $('header').removeClass('active_bg');
+            $('.title p').css('color', '#fff');
+
             }
         }
 
@@ -45,43 +48,43 @@ $(function(){
       // about me 
       var s1=$('#aboutme').offset().top;
       if(ws > s1-400){
-        $('#aboutme').css('background-color', '#eee') //gray 색상
+        $('#aboutme').css('background-color', '#fff') //활성
                       .css('opacity', '1');
       }else {
-        $('#aboutme').css('background-color', '#fff') //비활성
+        $('#aboutme').css('background-color', '#eee') //비활성
                       .css('opacity', '0.5');
       }
       // skill
       var s2=$('#skill').offset().top;
       if(ws > s2-400){
-        $('#skill').css('background-color', '#eee') //gray 색상
+        $('#skill').css('background-color', '#fff') //흰바탕 활성
                       .css('opacity', '1');
-        $('#aboutme').css('background-color', '#fff') //비활성
+        $('#aboutme').css('background-color', '#eee') //비활성
                       .css('opacity', '0.5');
       }else {
-        $('#skill').css('background-color', '#fff') //비활성
+        $('#skill').css('background-color', '#eee') //비활성
                       .css('opacity', '0.5');
       }
       // work
       var s3=$('#work').offset().top;
       if(ws > s3-400){
-        $('#work').css('background-color', '#eee') //gray 색상
+        $('#work').css('background-color', '#fff') //흰바탕 활성
                       .css('opacity', '1');
-        $('#skill').css('background-color', '#fff') //비활성
+        $('#skill').css('background-color', '#eee') //비활성
                       .css('opacity', '0.5');
       }else {
-        $('#work').css('background-color', '#fff') //비활성
+        $('#work').css('background-color', '#eee') //비활성
                       .css('opacity', '0.5');
       }
       // contact
       var s4=$('#contact').offset().top;
       if(ws > s4-400){
-        $('#contact').css('background-color', '#eee') //gray 색상
+        $('#contact').css('background-color', '#fff') //흰바탕 활성
                       .css('opacity', '1');
-        $('#work').css('background-color', '#fff') //비활성
+        $('#work').css('background-color', '#eee') //비활성
                       .css('opacity', '0.5');
       }else {
-        $('#contact').css('background-color', '#fff') //비활성
+        $('#contact').css('background-color', '#eee') //비활성
                       .css('opacity', '0.5');
       }
   });
